9e207d3f59aad5c00fe457196554056cb8348579
vikash <vikash.tomar.india@gmail.com>
2023-08-09 23:59:32 +0530

    sec

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -12,6 +12,9 @@ function Home() {
           src="https://plus.unsplash.com/premium_photo-1675129779582-d84b954f2397?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
           alt=""
         />
+
+        <h1>hello </h1>
+  
       </div>
     </div>
   );
