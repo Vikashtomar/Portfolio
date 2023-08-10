25e8720ba7520d49bd37e1643a7b41edbdbb9791
vikash <vikash.tomar.india@gmail.com>
2023-08-10 20:13:04 +0530

    update first route

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,22 +1,35 @@
-/** @format */
 
 import React from "react";
 import "./home.css";
 
 function Home() {
   return (
+    <>
+    <div className="parent">
+
     <div className="man">
-      <div className="photo">
-        <h1>Home section</h1>
+      
+
+      <div className="photo">      
         <img
           src="https://plus.unsplash.com/premium_photo-1675129779582-d84b954f2397?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
-          alt=""
-        />
-
-        <h1>hello </h1>
-  
+          alt="aadmi"
+        />  
       </div>
-    </div>
+      
+    <div className="name"> 
+
+    <h1>  Vikash Tomar</h1>
+    <h2>Web Developer</h2>
+
+    <button>RESUME</button> 
+    <button>HIRE ME</button>
+    
+    
+  </div>
+      </div>
+          </div>
+    </>
   );
 }
 
