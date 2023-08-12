a033994784b4e4b59b36bffb58b36adc3dcf8cdd
vikash <vikash.tomar.india@gmail.com>
2023-08-12 23:54:30 +0530

    second commit

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -7,8 +7,8 @@ function Work() {
       
       <p>JUN,2022 - PRESENT </p>          
       <div className="workcontent">
-            <h3>PANAPPS INC </h3>
-            <h5>Software Developer</h5> 
+             <p> PANAPPS INC </p>
+            <p>Software Developer</p>
             <p>
       - Develop, test and implement new software programs with vue js and dotnet
 - Clearly and regularly communicate with management and technical support colleagues
@@ -21,43 +21,50 @@ function Work() {
     </div>
     </div>
 
-<div className='work'>
-      
-<p>JUN,2022 - PRESENT </p>          
-<div className="workcontent">
-      <h3>PANAPPS INC </h3>
-      <h5>Software Developer</h5> 
-      <p>
-- Develop, test and implement new software programs with vue js and dotnet
-- Clearly and regularly communicate with management and technical support colleagues
-- Design and update software database. This includes, but is not limited to: Software Applications, Web Sites, Data Communication Processes and User Interfaces
-- Analyze user requirements and convert requirements to design documents
-- Work with experienced team members to conduct root cause analysis of issues, review new and existing code and/or perform unit testing
-     </p> 
-   
- 
-</div>
-</div>
+{/* //sec */}
 
-
+{/* //second */}
 
 <div className='work'>
       
-      <p>JUN,2022 - PRESENT </p>          
+      <p>SEP,2021 - FEB,2022 </p>          
       <div className="workcontent">
-            <h3>PANAPPS INC </h3>
-            <h5>Software Developer</h5> 
+            <p> TORC INFOTECH </p>
+          <p>Assosiate Software Developer</p>
             <p>
-      - Develop, test and implement new software programs with vue js and dotnet
-- Clearly and regularly communicate with management and technical support colleagues
-- Design and update software database. This includes, but is not limited to: Software Applications, Web Sites, Data Communication Processes and User Interfaces
-- Analyze user requirements and convert requirements to design documents
-- Work with experienced team members to conduct root cause analysis of issues, review new and existing code and/or perform unit testing
+            - Developed web pages using html css bootstrap javascript react and django
+- Implemented Client side validations using JavaScript
+- identifies the requirements of the clients and discusses it with the development manager
+- monitors software development process and supports the team members in all phases of the project
+- assists the clients in the installation, maintenance, troubleshooting and updating the software.
            </p> 
          
        
     </div>
     </div>
+
+
+
+
+
+    <div className='work'>
+      
+      <p>SEP,2020 - MAR,2021</p>          
+      <div className="workcontent">
+          <p>STROKX TECHNOLOGIES</p>
+          <p>Frondend Developer</p>
+            <p>
+            - Create web application front end as per design comps and information architecture
+- Work with other developers to ensure that the website is functional and visually appealing
+- Collect feedback from design and technical staff on website development needs.
+- Plan the layout of the website
+- Ensure that the website is optimized for various devices
+           </p> 
+         
+       
+      </div>
+      </div>
+
     </>
   )
 }
